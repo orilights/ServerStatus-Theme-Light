cc9dcf31c429ed6a3ead30efea454d58836f00a0
OriLight <amatlsmisr@gmail.com>
2023-10-08 11:30:21 +0800

    refactor: 外置页头与页脚，方便直接修改相关信息

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <div class="px-3 sm:px-6 min-h-screen relative">
-    <h1 class="text-4xl font-bold py-4">
-      服务器监控
-    </h1>
-    <div v-if="loading" class="w-fit mx-auto my-2">
-      加载中
-    </div>
-    <div v-if="error" class="w-fit mx-auto my-2">
-      数据加载失败，请尝试刷新页面或检查 ServerStatus 服务端状态
-    </div>
-    <div v-if="serverData" class="flex flex-wrap gap-x-4 gap-y-3">
-      <ServerItem
-        v-for="server in serverData.servers" :key="server.name"
-        :server="server"
-        class="flex-1 min-w-[300px]"
-      />
-    </div>
-    <div class="h-16" />
-    <div class="text-center absolute left-0 bottom-0 py-4 w-full">
-      Powered by <a class="text-blue-500" href="https://github.com/zdz/ServerStatus-Rust" target="_blank">ServerStatus-Rust</a>. Theme <a class="text-blue-500" href="https://github.com/orilights/ServerStatus-Theme-Light" target="_blank">Light</a> by <a class="text-blue-500" href="https://orilight.top/" target="_blank">OriLight</a>
-    </div>
+  <div v-if="loading" class="w-fit mx-auto my-2">
+    加载中
   </div>
+  <div v-if="error" class="w-fit mx-auto my-2">
+    数据加载失败，请尝试刷新页面或检查 ServerStatus 服务端状态
+  </div>
+  <div v-if="serverData" class="flex flex-wrap gap-x-4 gap-y-3">
+    <ServerItem
+      v-for="server in serverData.servers" :key="server.name"
+      :server="server"
+      class="flex-1 min-w-[300px]"
+    />
+  </div>
+  <div class="h-16" />
 </template>
 
 <script setup lang="ts">
